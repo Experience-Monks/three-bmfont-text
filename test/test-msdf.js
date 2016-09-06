e2d386039910303c0aad8d1a60bb398a8b236baf
mattdesl <dave.des@gmail.com>
2016-09-06 17:52:35 -0400

    random colors and text for the msdf test

--- test/test-msdf.js
+++ test/test-msdf.js
@@ -10,8 +10,13 @@
 
 global.THREE = require('three')
 var createOrbitViewer = require('three-orbit-viewer')(THREE)
+var shuffle = require('array-shuffle')
+var quotes = shuffle(require('sun-tzu-quotes/quotes.json').join(' ').split('.'))
 var createText = require('../')
 var MSDFShader = require('../shaders/msdf')
+var palettes = require('nice-color-palettes')
+var palette = palettes[5]
+var background = palette.shift()
 
 require('./load')({
   font: 'fnt/Roboto-msdf.json',
@@ -20,7 +25,7 @@ require('./load')({
 
 function start (font, texture) {
   var app = createOrbitViewer({
-    clearColor: 'rgb(80, 80, 80)',
+    clearColor: background,
     clearAlpha: 1.0,
     fov: 65,
     position: new THREE.Vector3()
@@ -29,44 +34,55 @@ function start (font, texture) {
   app.camera = new THREE.OrthographicCamera()
   app.camera.left = 0
   app.camera.top = 0
-  app.camera.near = -100
-  app.camera.far = 100
+  app.camera.near = -1
+  app.camera.far = 1000
 
-  var geom = createText({
-    text: 'This text is rendered with Multi-Channel Signed Distance Fields.\n\n' +
-      'The quick brown fox jumps over the lazy dog.',
-    font: font,
-    align: 'left',
-    width: 500,
-    flipY: texture.flipY
-  })
-
-  var material = new THREE.RawShaderMaterial(MSDFShader({
-    map: texture,
-    transparent: true,
-    color: 'rgb(230, 230, 230)'
-  }))
-
-  var layout = geom.layout
-  var text = new THREE.Mesh(geom, material)
-  var padding = 40
-  text.position.set(padding, -layout.descender + layout.height + padding, 0)
-
-  var textAnchor = new THREE.Object3D()
-  textAnchor.add(text)
-  app.scene.add(textAnchor)
+  var container = new THREE.Object3D()
+  app.scene.add(container)
+  var count = 100
+  for (var i = 0; i < count; i++) {
+    createGlyph()
+  }
 
   var time = 0
   // update orthographic
   app.on('tick', function (dt) {
     time += dt / 1000
     var s = (Math.sin(time * 0.5) * 0.5 + 0.5) * 2.0 + 0.5
-    textAnchor.scale.set(s, s, s)
+    container.scale.set(s, s, s)
     // update camera
     var width = app.engine.width
     var height = app.engine.height
-    app.camera.right = width
-    app.camera.bottom = height
+    app.camera.left = -width / 2
+    app.camera.right = width / 2
+    app.camera.top = -height / 2
+    app.camera.bottom = height / 2
     app.camera.updateProjectionMatrix()
   })
+
+  function createGlyph () {
+    var angle = (Math.random() * 2 - 1) * Math.PI
+    var geom = createText({
+      text: quotes[Math.floor(Math.random() * quotes.length)].split(/\s+/g).slice(0, 3).join(' '),
+      font: font,
+      align: 'left',
+      flipY: texture.flipY
+    })
+
+    var material = new THREE.RawShaderMaterial(MSDFShader({
+      map: texture,
+      transparent: true,
+      color: palette[Math.floor(Math.random() * palette.length)]
+    }))
+
+    var layout = geom.layout
+    var text = new THREE.Mesh(geom, material)
+    text.position.set(0, -layout.descender + layout.height, 0)
+    text.scale.multiplyScalar(Math.random() * 1.5 + 0.05)
+
+    var textAnchor = new THREE.Object3D()
+    textAnchor.add(text)
+    textAnchor.rotation.z = angle
+    container.add(textAnchor)
+  }
 }
