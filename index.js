83d11ac2fe4cda9333a939b158bc7ab404a74c44
johnwildauer <johnwildauer@gmail.com>
2021-04-24 18:05:49 -0500

    rollback refactor and update to separate branch

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var createLayout = require('layout-bmfont-text')
+var inherits = require('inherits')
 var createIndices = require('quad-indices')
 
 var vertices = require('./lib/vertices')
@@ -6,120 +7,118 @@ var utils = require('./lib/utils')
 
 var Base = THREE.BufferGeometry
 
-module.exports = function createTextGeometry (opt) {
-  return new TextGeometry(opt)
+module.exports = function createTextGeometry(opt) {
+	return new TextGeometry(opt)
 }
-class TextGeometry extends Base {
-  constructor (opt) {
-    super()
 
-    this.type = 'TextGeometry'
+function TextGeometry(opt) {
+	Base.call(this)
 
-    if (typeof opt === 'string') {
-      this.parameters = {
-        opt: opt
-      }
-    }
-    // use these as default values for any subsequent
-    // calls to update()
-    this._opt = Object.assign({}, opt)
+	if (typeof opt === 'string') {
+		opt = { text: opt }
+	}
 
-    // also do an initial setup...
-    if (opt) this.update(opt)
-  }
+	// use these as default values for any subsequent
+	// calls to update()
+	this._opt = Object.assign({}, opt)
 
-  update (opt) {
-    if (typeof opt === 'string') {
-      opt = { text: opt }
-    }
+	// also do an initial setup...
+	if (opt) this.update(opt)
+}
 
-    // use constructor defaults
-    opt = Object.assign({}, this._opt, opt)
+inherits(TextGeometry, Base)
 
-    if (!opt.font) {
-      throw new TypeError('must specify a { font } in options')
-    }
+TextGeometry.prototype.update = function (opt) {
+	if (typeof opt === 'string') {
+		opt = { text: opt }
+	}
 
-    this.layout = createLayout(opt)
+	// use constructor defaults
+	opt = Object.assign({}, this._opt, opt)
 
-    // get vec2 texcoords
-    var flipY = opt.flipY !== false
+	if (!opt.font) {
+		throw new TypeError('must specify a { font } in options')
+	}
 
-    // the desired BMFont data
-    var font = opt.font
+	this.layout = createLayout(opt)
 
-    // determine texture size from font file
-    var texWidth = font.common.scaleW
-    var texHeight = font.common.scaleH
+	// get vec2 texcoords
+	var flipY = opt.flipY !== false
 
-    // get visible glyphs
-    var glyphs = this.layout.glyphs.filter(function (glyph) {
-      var bitmap = glyph.data
-      return bitmap.width * bitmap.height > 0
-    })
+	// the desired BMFont data
+	var font = opt.font
 
-    // provide visible glyphs for convenience
-    this.visibleGlyphs = glyphs
+	// determine texture size from font file
+	var texWidth = font.common.scaleW
+	var texHeight = font.common.scaleH
 
-    // get common vertex data
-    var positions = vertices.positions(glyphs)
-    var uvs = vertices.uvs(glyphs, texWidth, texHeight, flipY)
-    var indices = createIndices([], {
-      clockwise: true,
-      type: 'uint16',
-      count: glyphs.length
-    })
+	// get visible glyphs
+	var glyphs = this.layout.glyphs.filter(function (glyph) {
+		var bitmap = glyph.data
+		return bitmap.width * bitmap.height > 0
+	})
 
-    // update vertex data
-    this.setIndex(indices)
-    this.setAttribute('position', new THREE.BufferAttribute(positions, 2))
-    this.setAttribute('uv', new THREE.BufferAttribute(uvs, 2))
+	// provide visible glyphs for convenience
+	this.visibleGlyphs = glyphs
 
-    // update multipage data
-    if (!opt.multipage && 'page' in this.attributes) {
-      // disable multipage rendering
-      this.removeAttribute('page')
-    } else if (opt.multipage) {
-      // enable multipage rendering
-      var pages = vertices.pages(glyphs)
-      this.setAttribute('page', new THREE.BufferAttribute(pages, 1))
-    }
-  }
+	// get common vertex data
+	var positions = vertices.positions(glyphs)
+	var uvs = vertices.uvs(glyphs, texWidth, texHeight, flipY)
+	var indices = createIndices([], {
+		clockwise: true,
+		type: 'uint16',
+		count: glyphs.length,
+	})
 
-  computeBoundingSphere () {
-    if (this.boundingSphere === null) {
-      this.boundingSphere = new THREE.Sphere()
-    }
+	// update vertex data
+	this.setIndex(indices)
+	this.setAttribute('position', new THREE.BufferAttribute(positions, 2))
+	this.setAttribute('uv', new THREE.BufferAttribute(uvs, 2))
 
-    var positions = this.attributes.position.array
-    var itemSize = this.attributes.position.itemSize
-    if (!positions || !itemSize || positions.length < 2) {
-      this.boundingSphere.radius = 0
-      this.boundingSphere.center.set(0, 0, 0)
-      return
-    }
-    utils.computeSphere(positions, this.boundingSphere)
-    if (isNaN(this.boundingSphere.radius)) {
-      console.error(
-				`THREE.BufferGeometry.computeBoundingSphere():
-Computed radius is NaN. The
-"position" attribute is likely to have NaN values.`
-      )
-    }
-  }
+	// update multipage data
+	if (!opt.multipage && 'page' in this.attributes) {
+		// disable multipage rendering
+		this.removeAttribute('page')
+	} else if (opt.multipage) {
+		// enable multipage rendering
+		var pages = vertices.pages(glyphs)
+		this.setAttribute('page', new THREE.BufferAttribute(pages, 1))
+	}
+}
 
-  computeBoundingBox () {
-    if (this.boundingBox === null) {
-      this.boundingBox = new THREE.Box3()
-    }
+TextGeometry.prototype.computeBoundingSphere = function () {
+	if (this.boundingSphere === null) {
+		this.boundingSphere = new THREE.Sphere()
+	}
 
-    var bbox = this.boundingBox
-    var positions = this.attributes.position.array
-    var itemSize = this.attributes.position.itemSize
-    if (!positions || !itemSize || positions.length < 2) {
-      bbox.makeEmpty()
-      return
-    }
-    utils.computeBox(positions, bbox)
-  }
+	var positions = this.attributes.position.array
+	var itemSize = this.attributes.position.itemSize
+	if (!positions || !itemSize || positions.length < 2) {
+		this.boundingSphere.radius = 0
+		this.boundingSphere.center.set(0, 0, 0)
+		return
+	}
+	utils.computeSphere(positions, this.boundingSphere)
+	if (isNaN(this.boundingSphere.radius)) {
+		console.error(
+			'THREE.BufferGeometry.computeBoundingSphere(): ' +
+				'Computed radius is NaN. The ' +
+				'"position" attribute is likely to have NaN values.'
+		)
+	}
+}
+
+TextGeometry.prototype.computeBoundingBox = function () {
+	if (this.boundingBox === null) {
+		this.boundingBox = new THREE.Box3()
+	}
+
+	var bbox = this.boundingBox
+	var positions = this.attributes.position.array
+	var itemSize = this.attributes.position.itemSize
+	if (!positions || !itemSize || positions.length < 2) {
+		bbox.makeEmpty()
+		return
+	}
+	utils.computeBox(positions, bbox)
 }
